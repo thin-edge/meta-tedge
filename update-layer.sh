8b4cb1b69c5856c7a2adb3619d70a6d03d6a7b57
Ruadhri17 <k.r.piotrowski17@gmail.com>
2022-08-29 14:51:58 +0200

    correct filenames; add license md5sum

--- update-layer.sh
+++ update-layer.sh
@@ -20,7 +20,27 @@ for package in recipes-tedge/*; do
     cd "$package_dir" || exit
 
     cargo bitbake
+
     mv *.bb "$layer_dir/recipes"
 
     cd - || exit
 done
+
+# Current md5 sum of License file
+md5_sum=`md5sum $thin_edge_path/LICENSE.txt | cut -f 1 -d " "`
+
+cd recipes
+
+# Replace the filename compatible with Yocto versioning convention 
+find . -type f -name '*.bb' | while read file ; do
+new_file="$(echo ${file} | sed -r 's/_/-/g ; s|-([^-]*)$|\_\1|')" ;
+
+[ "${file}" != "${new_file}" ] && mv "${file}" "${new_file}" ;
+done 
+
+# Fill the license field
+sed -r -i "s@Apache-2.0;md5=generateme@LICENSE.txt;md5=$md5_sum@" *.bb
+
+cd -
+
+
